413d2ee8c8210258d1e5136ac84c9949ab15c1df
etienne1698 <etienne.robert1698@gmail.com>
2024-11-19 21:51:38 +0100

    update

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,7 +1,27 @@
-export const degeesToRadiant = (deg: number) => deg * (Math.PI / 180);
-
 import { vec3, vec4, mat4 } from "gl-matrix";
 import { Camera } from "../models/camera";
+
+export const degeesToRadiant = (deg: number) => deg * (Math.PI / 180);
+
+export function isEqual(var1: any, var2: any): boolean {
+  if (var1 == null || var2 == null) return var1 == var2;
+  if (typeof var1 != typeof var2) return false;
+  if (
+    typeof var1 === "string" ||
+    typeof var1 === "boolean" ||
+    typeof var1 === "number" ||
+    typeof var1 === "bigint" ||
+    typeof var1 === "undefined"
+  ) {
+    return var1 === var2;
+  }
+  if (typeof var1 === "object") {
+    return Object.entries(var1).reduce((prev, [key, value]) => {
+      return prev && isEqual(value, var2[key]);
+    }, true);
+  }
+  return false;
+}
 
 export function getRayFromMouse(
   x: number,
@@ -9,11 +29,10 @@ export function getRayFromMouse(
   canvas: HTMLCanvasElement,
   camera: Camera
 ) {
-
   const canvasRect = canvas.getBoundingClientRect();
   const normalizedX = ((x - canvasRect.left) / canvasRect.width) * 2 - 1;
   const normalizedY = -(((y - canvasRect.top) / canvasRect.height) * 2 - 1);
-  
+
   // Convertir en espace de vue
   const inverseProjectionView = camera.viewProjectionMatrix;
   mat4.invert(inverseProjectionView, inverseProjectionView);
